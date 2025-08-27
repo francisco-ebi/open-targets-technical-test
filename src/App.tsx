aaa1b357575b725250a00e8b60a9d2fca25ba166
José Francisco Peña <jose.francisco.pees@gmail.com>
2025-08-27 15:06:44 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Overall highest drug targets associated with lung carcinoma</h1>
+    </>
+  );
+}
+
+export default App;
