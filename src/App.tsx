118a0bda2099fa681582b02856deb8bc268be341
José Francisco Peña <jose.francisco.pees@gmail.com>
2025-08-27 17:31:25 +0100

    WIP: expandable row and chart selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { getTargetsQuery } from "@/hooks/get-targets.query";
+import type { Target } from "@/models";
 import "./App.css";
 
 function App() {
   const { loading, error, targets } = getTargetsQuery();
+  const [selectedTarget, setSelectedTarget] = useState<Target | null>(null);
   console.log({ loading, error, targets });
+
   return (
     <>
       <h1>
@@ -20,19 +23,38 @@ function App() {
         </thead>
         <tbody>
           {targets.map((targetData) => (
-            <tr>
-              <td>
-                <a
-                  target="_blank"
-                  rel="noopener"
-                  href={`https://platform.opentargets.org/target/${targetData.target.id}`}
-                >
-                  {targetData.target.approvedSymbol}
-                </a>
-              </td>
-              <td>{targetData.target.approvedName}</td>
-              <td>{targetData.score.toFixed(5)}</td>
-            </tr>
+            <>
+              <tr key={targetData.target.id}>
+                <td className="selection-cell">
+                  <a
+                    target="_blank"
+                    rel="noopener"
+                    href={`https://platform.opentargets.org/target/${targetData.target.id}`}
+                  >
+                    {targetData.target.approvedSymbol}
+                  </a>
+                  <button
+                    className="outline open-row-button"
+                    onClick={() => setSelectedTarget(targetData)}
+                    aria-label={`View data charts for gene ${targetData.target.approvedSymbol}`}
+                  >
+                    +
+                  </button>
+                </td>
+                <td>{targetData.target.approvedName}</td>
+                <td>{targetData.score.toFixed(5)}</td>
+              </tr>
+              {targetData.target.id === selectedTarget?.target.id && (
+                <tr>
+                  <td colSpan={3}>
+                    <div className="tabs" role="group">
+                      <button className="outline">Bar chart</button>
+                      <button className="outline">Radar chart</button>
+                    </div>
+                  </td>
+                </tr>
+              )}
+            </>
           ))}
         </tbody>
       </table>
