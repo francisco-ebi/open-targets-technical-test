5405a99786339e5a59177f3cc65c21513c4fb095
José Francisco Peña <jose.francisco.pees@gmail.com>
2025-08-27 16:49:02 +0100

    feat: add initial version of targets table

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,27 @@ function App() {
   console.log({ loading, error, targets });
   return (
     <>
-      <h1>Overall highest drug targets associated with lung carcinoma</h1>
+      <h1>
+        Overall highest scored drug targets associated with lung carcinoma
+      </h1>
+      <table className="striped">
+        <thead>
+          <tr>
+            <th>Approved Symbol</th>
+            <th>Gene Name</th>
+            <th>Overall Association Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {targets.map((targetData) => (
+            <tr>
+              <td>{targetData.target.approvedSymbol}</td>
+              <td>{targetData.target.approvedName}</td>
+              <td>{targetData.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
